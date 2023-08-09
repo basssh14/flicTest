9cba06b794dae42ec29d0c035dd0b76ae75e4b82
Sebastian Velasco <sebastianvelasco@basssh14.local>
2023-08-09 14:43:56 -0500

    Create a test way to check there is no duplicate instances

--- example/lib/Copy.dart
+++ example/lib/Copy.dart
@@ -1,122 +1,38 @@
-import 'dart:io';
-import 'package:flic_button_example/Copy.dart';
-import 'package:flic_button_example/notification_service.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flic_button/flic_button.dart';
-import 'package:flutter/services.dart';
+import 'package:flic_button_example/main.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'Test.dart';
 
-void main() async {
-  //Last added
-  // Ensure that the Flutter binding is initialized before running the app
-  WidgetsFlutterBinding.ensureInitialized();
-  // Register the native part of the plugin
-  const platform = MethodChannel('your_channel_name');
-
-  platform.setMethodCallHandler((call) async {
-    if (call.method == 'showLocalNotification') {
-      print("channel method workssss!!!!!");
-      // await notificationService.showLocalNotification(
-      //   id: 0,
-      //   title: "Drink Water",
-      //   body: "Time to drink some water!",
-      //   payload: "You just took water! Huurray!",
-      // );
-    }
-  });
-
-  runApp(ChangeNotifierProvider(
-      create: (context) => CounterProvider(), child: MyApp()));
-}
-
-class MyApp extends StatefulWidget {
-  final MethodChannel platform = MethodChannel('flic_button_channel');
+class Copy extends StatefulWidget {
+  // final CounterProvider counterProvider;
+  const Copy({Key? key}) : super(key: key);
   @override
-  _MyAppState createState() => _MyAppState();
+  State<Copy> createState() => _CopyState();
 }
 
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: FlicButtonPage());
-  }
-}
-
-class FlicButtonPage extends StatefulWidget {
-  //final MethodChannel platform = MethodChannel('flic_button_channel');
-  @override
-  _FlicButtonPageState createState() => _FlicButtonPageState();
-}
-
-class _FlicButtonPageState extends State<FlicButtonPage> {
-  //To delete
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
-// Initialize the MethodChannel
-  static const platform = const MethodChannel('your_channel_name');
-  // the plugin manager to use while we are active
+class _CopyState extends State<Copy> {
   // Initialize the FlicButtonManagerSingleton
-  var counterProvider;
-  var flicButtonManager;
-  void initializeProvider() {
-    counterProvider = Provider.of<CounterProvider>(context, listen: false);
-    flicButtonManager = counterProvider.flicButtonManager;
-  }
+  // var counterProvider;
+  // var flicButtonManager;
+  // void initializeProvider() {
+  //   counterProvider = Provider.of<CounterProvider>(context, listen: false);
+  //   flicButtonManager = counterProvider.flicButtonManager;
+  // }
+  //
+  // @override
+  // void initState() {
+  //   initializeProvider();
+  //   super.initState();
+  // }
 
-  var notificationService;
-
-  @override
-  void initState() {
-    // create the FLIC 2 manager and initialize it
-    initializeProvider();
-    notificationService = NotificationService();
-    listenToNotificationStream();
-    notificationService.initializePlatformNotifications();
-    super.initState();
-    // Listen to the MethodChannel messages
-    platform.setMethodCallHandler((call) async {
-      print("methoddddddddddddd3434343434433");
-      print(call.method);
-      if (call.method == 'showLocalNotification') {
-        // Handle the restartApp message and send the notification
-        // Send notification
-        await notificationService.showLocalNotification(
-          id: 0,
-          title: "Drink Water",
-          body: "Time to drink some water!",
-          payload: "You just took water! Huurray!",
-        );
-      }
-    });
-    //Last added
-  }
-
-  void listenToNotificationStream() =>
-      notificationService.behaviorSubject.listen((payload) {
-        print("paylod");
-        print(payload);
-      });
-
-  //New stuff
   //Last added
-  void showLocalNotification(BuildContext context) async {
-    print("Gets in the showLocalNotification inside main.dart");
-    try {
-      await platform.invokeMethod('showLocalNotification', null);
-    } on PlatformException catch (e) {
-      print("Failed to invoke method: ${e.message}");
-    }
-  }
-  //Last added
-
   @override
   Widget build(BuildContext context) {
     return Consumer<CounterProvider>(
       builder: (context, model, child) => Scaffold(
         appBar: AppBar(
-          title: const Text('Flic Button Plugin Example'),
+          title: const Text('Flic Button Plugin Example 22'),
         ),
         body: FutureBuilder(
           future: model.flicButtonManager?.invokation,
@@ -221,7 +137,7 @@ class _FlicButtonPageState extends State<FlicButtonPage> {
           onPressed: () {
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => const Copy()),
+              MaterialPageRoute(builder: (context) => MyApp()),
             );
           },
           backgroundColor: Colors.green,
